cc33d8a7be5b2ea3058c01b943b6b98f1740f876
Alya <4bitFox@tschudibacon.com>
2026-09-14 08:26:59 +0200

    nixos-rebuild switch

--- modules/networking.nix
+++ modules/networking.nix
@@ -6,14 +6,14 @@
   networking = {
     networkmanager = {
       enable = true;
-      dns = "none"; # Manually set DNS nameservers instead.
+#      dns = "none"; # Manually set DNS nameservers instead.
       wifi.powersave = true;
     };
     ### manual DNS nameservers ###
     # useDHCP = false;
     # dhcpcd.enable = false;
-    nameservers = [
-      "127.0.0.1" # localhost (probably pihole)
+#    nameservers = [
+#      "127.0.0.1" # localhost (probably pihole)
     #  "1.1.1.1" # Cloudflare
     #  "1.0.0.1" # Cloudflare
     ];
