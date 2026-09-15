f89001047e215a109ccbdb5243f198e741c84f31
Alya <4bitFox@tschudibacon.com>
2026-09-15 08:26:09 +0200

    nixos-rebuild boot

--- modules/networking.nix
+++ modules/networking.nix
@@ -6,17 +6,17 @@
   networking = {
     networkmanager = {
       enable = true;
-#      dns = "none"; # Manually set DNS nameservers instead.
+#      dns = "none"; # Manually set DNS nameservers instead. ### DISABLED TEMPORARILY BECAUSE EDUROAM... :c
       wifi.powersave = true;
     };
     ### manual DNS nameservers ###
     # useDHCP = false;
     # dhcpcd.enable = false;
-#    nameservers = [
-#      "127.0.0.1" # localhost (probably pihole)
+#    nameservers = [ ### DISABLED TEMPORARILY BECAUSE EDUROAM... :c
+#      "127.0.0.1" # localhost (probably pihole) ### DISABLED TEMPORARILY BECAUSE EDUROAM... :c
     #  "1.1.1.1" # Cloudflare
     #  "1.0.0.1" # Cloudflare
-#    ];
+#    ]; ### DISABLED TEMPORARILY BECAUSE EDUROAM... :c
     firewall = {
       enable = true;
       allowedTCPPorts = [ ];
